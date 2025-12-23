3b14c48b94c508d95a9da5093ddc8d0e6013653a
Aathreya Kadambi <aathreyakadambi@berkeley.edu>
2025-12-23 14:45:53 -0500

    forgot to add these files

---
+++ src/types/blog.ts
@@ -0,0 +1,38 @@
+export const categories = [
+  "math",
+  "geometry",
+  "probability",
+  "ml",
+  "cs",
+  "philosophy",
+  "music",
+  "life",
+  "plants",
+  "science",
+  "tea",
+  "nt",
+  "artistic",
+] as const;
+
+export type Category = typeof categories[number];
+
+export type BlogPost = {
+    title: string;
+    content: string;
+    link: string;
+    date: string;
+    backgroundImage?: string; // Optional background image for each blog post
+    category: Category[];
+};
+
+export type BlogPageProps = {
+    posts: BlogPost[];
+};
+
+export type BlogPostCardProps = {
+    title: string;
+    content: string;
+    link: string;
+    date: string;
+    backgroundImage?: string; // Optional background image for each blog post
+};
